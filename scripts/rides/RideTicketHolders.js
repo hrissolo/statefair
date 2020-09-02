42745538849f3bd68472a7869870fa929448feae
hrissolo <hrissolo@gmail.com>
2020-09-02 14:01:29 -0500

    pushing

---
+++ scripts/rides/RideTicketHolders.js
@@ -0,0 +1,10 @@
+const contentTarget = document.querySelector(".rides")
+const eventHub = document.querySelector("#state-fair")
+
+export const RideTicketHolders = () => {
+    eventHub.addEventListener("rideTicketPurchased", customEvent => {
+        // console.log("Hello")
+        
+    })
+}
+
